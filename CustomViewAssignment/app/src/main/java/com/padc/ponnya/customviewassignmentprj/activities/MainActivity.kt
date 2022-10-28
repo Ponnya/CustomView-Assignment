aad7e1dcc6ec221fde7eef98920f86d255c74b95
Ponnya <mr.ponnya@gmail.com>
2022-10-28 18:03:10 +0630

    -Created Home Screen(Raw)

--- CustomViewAssignment/app/src/main/java/com/padc/ponnya/customviewassignmentprj/activities/MainActivity.kt
+++ CustomViewAssignment/app/src/main/java/com/padc/ponnya/customviewassignmentprj/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.padc.ponnya.customviewassignmentprj
+package com.padc.ponnya.customviewassignmentprj.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.padc.ponnya.customviewassignmentprj.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
