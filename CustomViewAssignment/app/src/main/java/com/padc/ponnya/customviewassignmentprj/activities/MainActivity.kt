9070e2e7247441310e96d4252e182354686a63bc
Ponnya <mr.ponnya@gmail.com>
2022-10-29 14:27:57 +0630

    -Overlap items right to left(RecyclerViewProfileImage)

--- CustomViewAssignment/app/src/main/java/com/padc/ponnya/customviewassignmentprj/activities/MainActivity.kt
+++ CustomViewAssignment/app/src/main/java/com/padc/ponnya/customviewassignmentprj/activities/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.padc.ponnya.customviewassignmentprj.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.padc.ponnya.customviewassignmentprj.R
 import com.padc.ponnya.customviewassignmentprj.adapters.ProfileImageAdapter
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,6 +22,23 @@ class MainActivity : BaseActivity() {
     private fun setUpRecyclerView() {
         mProfileImageAdapter = ProfileImageAdapter()
         rvProfileImage.adapter = mProfileImageAdapter
+        // Overlap Items Right to left
+        rvProfileImage.addItemDecoration(object : RecyclerView.ItemDecoration() {
+            override fun getItemOffsets(
+                outRect: Rect,
+                view: View,
+                parent: RecyclerView,
+                state: RecyclerView.State
+            ) {
+                val mPosition = parent.getChildAdapterPosition(view)
+                if (mPosition != 0)
+                    outRect.left = -80
+                else
+                    super.getItemOffsets(outRect, view, parent, state)
+
+            }
+        })
+
         rvProfileImage.layoutManager =
             LinearLayoutManager(applicationContext, LinearLayoutManager.HORIZONTAL, false)
 
